11837f5f06918050d01d677a39ab49a4f2c55233
renik <mrushvg@gmail.com>
2024-09-29 18:24:22 -0700

    set defaultoptions to fix a few key overlaps

--- kubejs/client_scripts/lang/lang.js
+++ kubejs/client_scripts/lang/lang.js
@@ -13,14 +13,25 @@ ClientEvents.lang('en_us', event => {
   	rename('ocean_recovery:plastic_shard', "Plastim Scrap")
   	rename('ocean_recovery:plastic_bag', "Plastim Bag")
   	rename('ocean_recovery:plastic_bottle', "Plastim Bottle")
+  	rename('ocean_recovery:sand_with_plastic', "Sand With Plastim")
+  	rename('ocean_recovery:gravel_with_plastic', "Gravel With Plastim")
+  	rename('ocean_recovery:plastic_glass', "Plastim Glass")
+  	rename('ocean_recovery:plastic_picker', "Specialized Plastim Picker")
+  	rename('ocean_recovery:diamond_plastic_picker', "Specialized Diamond Plastim Picker")
 
 // ICE AND FIRE
-  	rename('iceandfire:silver_ore', "Silver Ore 1")
-  	rename('iceandfire:deepslate_silver_ore', "Deepslate Silver Ore 1")
+  	rename('iceandfire:silver_ore', "Rich Silver Ore")
+  	rename('iceandfire:deepslate_silver_ore', "Rich Deepslate Silver Ore")
 
 // OCCULTISM
-  	rename('occultism:silver_ore', "Silver Ore 2")
-  	rename('occultism:silver_ore_deepslate', "Deepslate Silver Ore 2")
+  	rename('occultism:silver_ore', "Dense Silver Ore")
+  	rename('occultism:silver_ore_deepslate', "Dense Deepslate Silver Ore")
+
+// OCCULTISM
+  	rename('farmersdelight:tomato', "Roma Tomato")
+  	rename('farmersdelight:tomato_seeds', "Roma Tomato Seeds")
+  	rename('farmersdelight:cabbage', "Savoy Cabbage")
+  	rename('farmersdelight:cabbage_seeds', "Savoy Cabbage Seeds")
 
 
 
