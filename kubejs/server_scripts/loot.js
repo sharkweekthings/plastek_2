3845329a6af248bac1904b70f377fa8563910943
renik <mrushvg@gmail.com>
2024-11-17 10:50:42 -0800

    slight modify wisdom scroll drop rate

--- kubejs/server_scripts/loot.js
+++ kubejs/server_scripts/loot.js
@@ -2172,7 +2172,7 @@ LootJS.modifiers((e) => {
     .addWeightedLoot(
       [1], //1-3 rolls
       [
-  LootEntry.of("skilltree:wisdom_scroll").withWeight(15).limitCount([0, 1]),
+  LootEntry.of("skilltree:wisdom_scroll").withWeight(10).limitCount([0, 1]),
   LootEntry.of("kubejs:fake_wisdom_scroll").withWeight(15).limitCount([0, 1])	
       ]);
 
