da17f77be618404fac82239c707b74dba4484804
renik <mrushvg@gmail.com>
2025-03-27 23:19:20 -0700

    prep next build for create 0.6

--- kubejs/server_scripts/recipes/mysticalagriculture.js
+++ kubejs/server_scripts/recipes/mysticalagriculture.js
@@ -13,3 +13,26 @@ ServerEvents.recipes(event => {
 //
 })
 //
+
+//
+ServerEvents.recipes(event => {
+//
+
+  //ADD
+
+
+
+// Rubber Essence 1
+  event.shaped('2x industrialforegoing:dryrubber',
+    ['   ','111','   '], {
+    1: 'mysticalagriculture:rubber_essence'}).id('mysticalagriculture:rubber_essence')
+
+// Rubber Essence 2
+  event.shaped('2x thermal:cured_rubber',
+    ['111','   ','222'], {
+    1: 'mysticalagriculture:rubber_essence',
+    2: '#minecraft:coals'}).id('mysticalagriculture:rubber_essence2')
+
+//    
+})
+//
